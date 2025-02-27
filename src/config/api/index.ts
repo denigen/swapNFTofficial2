85a16ab7779b189100b07e42b65224291c951e3c
clumsybaby <162805278+denigen@users.noreply.github.com>
2025-02-26 20:20:48 -0500

    Delete src/config/api directory

--- src/config/api/index.ts
+++
@@ -1,9 +0,0 @@
-export * from './opensea';
-export * from './chains';
-
-export const API_CONFIG = {
-  OPENSEA: {
-    BASE_URL: 'https://api.opensea.io/api/v2',
-    TESTNET_URL: 'https://testnets-api.opensea.io/api/v2'
-  }
-} as const;
